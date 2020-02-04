140f658ec11b110937dd9317163d9f3385c7dba3
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-04 11:39:17 +0000

    Update accueil.inc.php

--- mvc/vue/fiche/accueil.inc.php
+++ mvc/vue/fiche/accueil.inc.php
@@ -8,38 +8,46 @@
 <div id='content2'>
     <div id='flex'>
         <div id='left-block'>
-            
+            <img src='./image/feuille.bmp' alt='' />
         </div>
         <div id='center-block'>
             <h1>Liste des fiche de competence</h1>
             <table id="tab">	  
                 <thead>
                     <tr>
+                        <!-- <th class="totab"> </th> -->
                         <th class="totab">Liste des <?php echo $nbFiches; ?> Fiche de competence disponible</th>
-                        <th class="totab"> </th>
-                        <th class="totab"> </th>
+                        <th class="totab"></th>
+                        <th class="totab"></th>
+                        <th class="totab"></th>
+                        <th class="totab"></th>
                     </tr>
                     <tr>
                         <th class="totab">Titre Fiche</th>
-                        <th class="totab">type fiche</th>
-                        <th class="totab">Disponible</th>
+                        <th class="totab">Type Fiche&nbsp;&nbsp;</th>
+                        <th class="totab">Disponible&nbsp;</th>
+                        <th class="totab">Nombre de competence&nbsp;</th>
+                        <th class="totab">Situation obligatoire&nbsp;</th>
                     </tr>
                 </thead>
                 <tbody>
-                        <?php 
-                        if (isset($listFiches)){
-                            while (!$listFiches->estVide()){
-                                    $uneFiche = $listFiches-> getUnElement();
-                        ?>
+                    <?php 
+                    if (isset($ListFiches)){
+                        while (!$ListFiches->estVide()){
+                                $uneFiche= $ListFiches->getUnElement();
+                    ?>
                     <tr class="ligne">
-                        <td class="titre"><?php echo $uneFiche->TitreFiche; ?></td>
-                        <td class="genre"><?php echo $uneFiche->TypeFiche; ?></td>
-                        <!-- <td class="annee"><?php echo $uneFiche->annee; ?></td> -->
+                        <td class="Titre"><?php echo $uneFiche->TitreFiche; ?></td>
+                        <td class="Type"><?php echo $uneFiche->TypeFiche; ?></td>
+                        <td class="Dispo"><?php echo ($uneFiche->Disponible==1) ? '&#x2713;': '&#x2717;'; ?></td>
+                        <td class="nbCompetence"><?php echo $uneFiche->nbCompetence; ?></td>
                     </tr>
-                    
                     <?php }}?>
                 </tbody>
     	    </table>
+        </div>
+        <div id='right-block'>
+            
         </div>
     </div><!--flex-->
 </div><!-- content2 -->
