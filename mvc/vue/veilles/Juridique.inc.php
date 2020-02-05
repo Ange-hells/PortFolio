b3530aca16b28ddbab5068f4f5120c20532a4db6
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-05 17:03:27 +0000

    create page
    create controller, modele and vue for technologie and juridique page

---
+++ mvc/vue/veilles/Juridique.inc.php
@@ -0,0 +1,20 @@
+<!-- ========= V U E =============================================================================================
+ fichier				: ./mvc/vue/projet/agenda.inc.php
+ auteur				    : Nathan Thomasset (nathan.thomasset2@gmail.com)
+ date de création	    : septembre 2019
+ date de modification   :
+ rôle					: permet de générer le code xhtml de la partie centrale de la page d'accueil du module cinepassion38
+<================================================================================================================== -->
+<div id='content2'>
+    <div id='flex'>
+        <div id='left-block'>
+            
+        </div>
+        <div id='center-block'>
+            <img src='./image/en-construction2.png' alt='' />
+        </div>
+        <div id='right-block'>
+            
+        </div>
+    </div><!--flex-->
+</div><!-- content2 -->
