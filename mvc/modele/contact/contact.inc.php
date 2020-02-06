a43e4b4108e558ae45b285062a21889b28c72126
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-06 16:47:05 +0000

    create contact page
    create modele, controller and vue for contact.contact

---
+++ mvc/modele/contact/contact.inc.php
@@ -0,0 +1,29 @@
+<?php
+class modeleContactContact extends modele{
+    
+    /**
+     * Renvoie le numéro du film à partir de son titre
+     * @param string $titreFilm : le titre du film
+     * @return integer : le numéro du film
+     */
+    public function getNbrFiches() {
+        $sql = "";
+        $pdoStat = $this->executerRequete($sql);
+        $nbfiche = $pdoStat->fetchObject();
+        return $nbfiche->nbFiches;
+    }
+        
+    public function getListFiche($i){
+        $collection = new collection();
+        $sql = "";
+
+        $pdoStat = $this->executerRequete($sql);
+        while(($uneFiche = $pdoStat->fetchObject()) !== false){
+            $collection->ajouter($uneFiche); 
+        }
+        return $collection;
+    }
+
+}
+
+?>
