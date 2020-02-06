5d5d16a7d785351efa183afc638d407953016cd7
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-06 16:49:51 +0000

    Update accueil.inc.php

--- mvc/vue/fiche/accueil.inc.php
+++ mvc/vue/fiche/accueil.inc.php
@@ -16,11 +16,7 @@
                 <thead>
                     <tr>
                         <!-- <th class="totab"> </th> -->
-                        <th class="totab">Liste des <?php echo $nbFiches; ?> Fiche de competence disponible</th>
-                        <th class="totab"></th>
-                        <th class="totab"></th>
-                        <th class="totab"></th>
-                        <th class="totab"></th>
+                        <th class="totab" colspan=5>Liste des <?php echo $nbFiches; ?> Fiche de competence disponible</th>
                     </tr>
                     <tr>
                         <th class="totab">Titre Fiche</th>
@@ -35,15 +31,16 @@
                     if (isset($ListFiches)){
                         while (!$ListFiches->estVide()){
                                 $uneFiche= $ListFiches->getUnElement();
+                                // var_dump($uneFiche)
                     ?>
                     <tr class="ligne">
                         <td class="Titre"><?php echo ($uneFiche->Disponible==0) ? '' : "<a href='./index.php?module=projet&amp;page=$uneFiche->TitreFiche'>";  echo $uneFiche->TitreFiche; "</a>"?></td>
                         <td class="Type"><?php echo $uneFiche->TypeFiche; ?></td>
                         <td class="Dispo"><?php echo ($uneFiche->Disponible==1) ? '&#x2713;': '&#x2717;'; ?></td>
                         <td class="nbCompetence"><?php echo $uneFiche->nbCompetence; ?></td>
-                        <td class="S.Obligatoire"><?php echo ($uneFiche->SituationObligatoire==0) ? '/':$uneFiche->SituationObligatoire; ?></td>
+                        <td class="S.Obligatoire"><?php echo ($uneFiche->LibelleSituation=="") ? '/':$uneFiche->LibelleSituation; ?></td>
                     </tr>
-                    <?php }}?>
+                    <?php }} ?>
                 </tbody>
     	    </table>
         </div>
