4a58bcf69ccbb6f45b6af49a126fefc90fb59039
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-07 16:18:56 +0000

    Update contact.inc.php
    create template for contact

--- mvc/vue/contact/contact.inc.php
+++ mvc/vue/contact/contact.inc.php
@@ -8,10 +8,23 @@
 <div id='content2'>
     <div id='flex'>
         <div id='left-block'>
-           
+            <div>
+                <p>
+                    <h1>Auteur :</h1><h3>Nathan THOMASSET</h3>
+                    <h1>Tel :</h1><h3>0782941983</h3>
+                    <h1>Mail :</h1><h3>nathan.thomasset2@gmail.com</h3>
+                </p>
+            </div>
+            <div> 
+                <p>
+                    <h1>WebDesigner :</h1><h3>Nathan THOMASSET</h3>
+                    <h1>Tel :</h1><h3>0782941983</h3>
+                    <h1>Mail :</h1><h3>nathan.thomasset2@gmail.com</h3>
+                </p> 
+            </div>
         </div>
         <div id='center-block'>
-            
+
         </div>
         <div id='right-block'>
             
