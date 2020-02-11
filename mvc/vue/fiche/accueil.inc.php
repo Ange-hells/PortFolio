d7d892e4275eac337a3090a9929bafcccae64898
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-11 16:58:39 +0000

    Update accueil.inc.php

--- mvc/vue/fiche/accueil.inc.php
+++ mvc/vue/fiche/accueil.inc.php
@@ -34,7 +34,7 @@
                                 // var_dump($uneFiche)
                     ?>
                     <tr class="ligne">
-                        <td class="Titre"><?php echo ($uneFiche->Disponible==0) ? '' : "<a href='./index.php?module=projet&amp;page=$uneFiche->TitreFiche'>";  echo $uneFiche->TitreFiche; "</a>"?></td>
+                        <td class="Titre"><?php echo ($uneFiche->Disponible==0) ? '' : "<a href='./index.php?module=projet&amp;page=$uneFiche->nomLien'>";  echo $uneFiche->TitreFiche; "</a>"?></td>
                         <td class="Type"><?php echo $uneFiche->TypeFiche; ?></td>
                         <td class="Dispo"><?php echo ($uneFiche->Disponible==1) ? '&#x2713;': '&#x2717;'; ?></td>
                         <td class="nbCompetence"><?php echo $uneFiche->nbCompetence; ?></td>
