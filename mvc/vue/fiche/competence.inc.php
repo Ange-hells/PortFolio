cd49f08377209461d394445494baabc82874d809
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-12 16:11:15 +0000

    Update competence.inc.php

--- mvc/vue/fiche/competence.inc.php
+++ mvc/vue/fiche/competence.inc.php
@@ -9,19 +9,19 @@
     <div id='flex'>
         <div id='left-block'>
             
-        </div>
+        </div><!--left-->
         <div id='center-block'>
-            <h1>Liste des fiche de competence</h1>
+            <h1>Liste des competences</h1>
             <table id="tab">	  
                 <thead>
                     <tr>
                         <!-- <th class="totab"> </th> -->
-                        <th class="totab" colspan=3>Liste des <?ph echo $nbFiches; ?> Competence disponible</th>
+                        <th class="totab" colspan=3>Liste des <?ph echo $nbCompetences;?> Competences disponible</th>
                     </tr>
                     <tr>
                         <th class="totab">Code</th>
                         <th class="totab">Libeller&nbsp;&nbsp;</th>
-                        <th class="totab">Presence&nbsp;</th>
+                        <th class="totab">Présent&nbsp;</th>
                         <!-- <th class="totab">Nombre de competence&nbsp;</th>
                         <th class="totab">Situation obligatoire&nbsp;</th> -->
                     </tr>
@@ -34,19 +34,19 @@
                                 // var_dump($uneFiche)
                     ?>
                     <tr class="ligne">
-                        <td class="Code"><?php echo $uneCompetence->codeCompetence; "</a>"?></td>
-                        <td class="Libeller"><?php echo $uneCompetence->libelleCompetence; ?></td>
-                        <td class="Presence"><?php echo $uneCompetence->Disponible; ?></td>
+                        <td class="Code"><?php echo $uneCompetence->codeCompetence;?></td>
+                        <td class="Libeller"><?php echo $uneCompetence->libelleCompetence;?></td>
+                        <td class="Present"><?php echo $uneCompetence->present, "&nbsp;" , ($uneCompetence->present==1) ? 'foi':'fois';?></td>
                         <!-- <td class="nbCompetence">?php echo $uneCompetence->nbCompetence; ?></td>
                         <td class="S.Obligatoire">?php echo ($uneCompetence->LibelleSituation=="") ? '/':$uneFiche->LibelleSituation; ?></td> -->
                     </tr>
                     <?php }} ?>
                 </tbody>
     	    </table>
-        </div>
+        </div><!--center-->
         <div id='right-block'>
             
-        </div>
+        </div><!--right-->
     </div><!--flex-->
 </div><!-- content2 -->
 
