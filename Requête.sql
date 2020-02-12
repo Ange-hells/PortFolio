144ba4b0f4f75f3b5f1b03cf76cd4a61b4f37b9c
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-12 16:10:54 +0000

    Update Requête.sql

--- Requête.sql
+++ Requête.sql
@@ -1,2 +1,16 @@
 USE portfolio;
 
+-- Fiche.accueil
+-- {
+        SELECT count(*) AS nbFiches 
+        FROM situation ;
+
+        SELECT s.LibelleSituation as TitreFiche, t.LibelleTypeFiche as TypeFiche, s.Disponible, 
+            count(NumAsso) as nbCompetence, so.LibelleSituation, nomLien
+        FROM situation s, typefiche t, competencesituation c, situationobligatoire so 
+        WHERE s.TypeFiche = t.IdTypeFiche
+        AND s.SituationObligatoire = so.IdSituation
+        AND c.NumSituation = s.NumSituation
+        AND s.NumSituation = '$i';
+-- }
+
