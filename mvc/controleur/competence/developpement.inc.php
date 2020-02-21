4a13fc7b253aaeba543b84ae71535d927fefbc96
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-21 10:14:46 +0000

    create developpement page

---
+++ mvc/controleur/competence/developpement.inc.php
@@ -0,0 +1,65 @@
+<?php
+/*======= C O N T R O L E U R ====================================================================================
+	fichier				: ./mvc/controleur/competence/developpement.inc.php
+	auteur				: Nathan Thomsset (nathan.thomasset2@gmail.com)
+	date de création	: fevrier 2020
+	date de modification:
+	rôle				: le contrôleur de la page de la fiche developpement
+  ================================================================================================================*/
+
+/**
+ * Classe contrôleur de la page accueil du domaine cinepassion38
+ * @author Sio-Slam <contact.dev@sio-slam.com>
+ * @version 1.0
+ * @copyright Sio-Slam - fevrier 2020
+ */
+class controleurCompetenceDeveloppement extends controleur {
+	
+	/**
+	 * Met à jour le tableau $donnees de la classe mère avec les informations spécifiques de la page
+	 * @param null
+	 * @return null
+	 * @author Sio-Slam <contact.dev@sio-slam.com>
+	 * @version 1.1
+	 * @copyright Sio-Slam - fevrier 2020
+	 */
+	public function setDonnees() {
+		// ===============================================================================================================
+		// titres de la page
+		// ===============================================================================================================
+		$this->titreHeader = "Page d'accueil developpement";
+		$this->titreMain = "";
+				
+		// ===============================================================================================================
+		// encarts
+		// ===============================================================================================================
+		// $this->encartsGauche = "partenaires.txt";
+	 	// $this->encartsDroite = "partenaires.txt";
+				
+		// ===============================================================================================================
+		// texte défilant
+		// ===============================================================================================================
+		// rien
+		
+		// ===============================================================================================================
+		// alimentation des données COMMUNES à toutes les pages
+		// ===============================================================================================================
+		parent::setDonnees();
+	}
+	
+	/**
+	 * Génère l'affichage de la vue pour l'action par défaut de la page 
+	 * @param null
+	 * @return null
+	 * @author Sio-Slam <contact.dev@sio-slam.com>
+	 * @version 1.0
+	 * @copyright Sio-Slam - fevrier 2020
+	 */
+	public function defaut() {
+
+
+		parent::genererVue();
+	}
+} // class
+
+?>
---
+++ mvc/vue/competence/developpement.inc.php
@@ -0,0 +1,21 @@
+<!-- ========= V U E =============================================================================================
+ fichier				: ./mvc/vue/competence/markdown.inc.php
+ auteur				    : Nathan Thomasset (nathan.thomasset2@gmail.com)
+ date de création	    : fevrier 2020
+ date de modification   :
+ rôle					: permet de générer le code html de la partie centrale de la page markdown
+<================================================================================================================== -->
+<div id='content2'>
+    <div id='flex'>
+        <div id='left-block'>
+            
+        </div>
+        <div id='center-block'>
+            
+        </div>
+        <div id='right-block'>
+            
+        </div>
+    </div><!--flex-->
+</div><!-- content2 -->
+
