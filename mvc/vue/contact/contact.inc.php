05775c4d8254aebff347e6c77f077a40b1e188a6
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-02-21 10:15:57 +0000

    Update contact.inc.php

--- mvc/vue/contact/contact.inc.php
+++ mvc/vue/contact/contact.inc.php
@@ -1,40 +1,52 @@
 <!-- ========= V U E =============================================================================================
- fichier				: ./mvc/vue/projet/agenda.inc.php
- auteur				: Nathan Thomasset (nathan.thomasset2@gmail.com)
- date de création	: septembre 2019
- date de modification:
- rôle					: permet de générer le code xhtml de la partie centrale de la page d'accueil du module cinepassion38
+ fichier				: ./mvc/vue/projet/contact.inc.php
+ auteur				    : Nathan Thomasset (nathan.thomasset2@gmail.com)
+ date de création	    : fevrier 2020
+ date de modification   :
+ rôle					: permet de générer le code html de la partie centrale de la page contact
 <================================================================================================================== -->
 <div id='content2'>
     <div id='flex'>
         <div id='left-block'>
-            <div class='carte'>
-                <div class='ligne'>
-                    <h1>Auteur :</h1><h3>Nathan THOMASSET</h3>
-                </div>
-                <div class='ligne'>
-                    <h1>Tel :</h1><h3>0782941983</h3>
-                </div>
-                <div class='ligne'>
-                    <h1>Mail :</h1><h3>nathan.thomasset2@gmail.com</h3>
-                </div>
-            </div>
-            <div class='carte'> 
-                <div>
-                    <div class='ligne'>
-                        <h1>WebDesigner :</h1><h3>Nathan THOMASSET</h3>
-                </div>
-                    <div class='ligne'>
-                        <h1>Tel :</h1><h3>0782941983</h3>
-                </div>
-                    <div class='ligne'>
-                        <h1>Mail :</h1><h3>nathan.thomasset2@gmail.com</h3>
-                </div>
-                </div> 
-            </div>
+            
         </div>
         <div id='center-block'>
-            
+            <div class='carte'>
+                <fieldset>
+                    <legend>Me contacter : </legend>
+                    <p><b>nom :</b> THOMASSET</p>
+                    <p><b>prénom :</b> Nathan</p>
+                    <p><b>e-mail :</b> nathan.thomasset2@gmail.com &nbsp;</p>
+                    <p><b>tel :</b> 07.82.94.19.83</p>
+                </fieldset>
+                <fieldset>
+                    <legend>Réseau : </legend>
+                    <a href="https://www.developpez.net/forums/u1524101/arceus/"><img src="./image/partenaire/developpez.png" /></a>
+                    <a href="https://github.com/Ange-hells"><img src="./image/partenaire/github.png" /></a>
+                    <a href="https://www.linkedin.com/in/nathan-thomasset-aa0b3112b"><img src="./image/partenaire/linkedin.png" /></a>
+                    <a href="https://my.indeed.com/p/nathant-haxw5hb"><img src="./image/partenaire/inded.png" /></a></br>
+                    <!-- <a href=""><img src="./image/partenaire/superprof.png" /></a> -->
+                    <a href="https://www.meteojob.com/candidate-console/profile"><img src="./image/partenaire/meteoJob.png" /></a>
+                    <!-- <a href=""><img src="./image/partenaire/cornerJob.png" /></a> -->
+                    <!-- <a href="" ><img src="./image/partenaire/folios.png" /></a> -->
+                </fieldset>
+            </div>
+            <div class='carte'>
+                <fieldset>
+                    <legend>Nous contacter : </legend>
+                    <p><b>nom :</b> SIO-SLAM</p>
+                    <p><b>e-mail :</b> contact.dev@sio-slam.com</p>
+                    <p><b>e-mail :</b> contact.Sio-Slam@sio-slam.com</p>
+                    <p><b>tel :</b> 09.54.84.15.30</p>
+                </fieldset>
+                <fieldset>
+                    <legend>Réseau : </legend>
+                    <a href="https://sio-slam.com"><img src="./image/partenaire/sio-slam.png" /></a>
+                    <a href="https://www.developpez.net/forums/u1524101/arceus/"><img src="./image/partenaire/developpez.png" /></a>
+                    <a href="https://github.com/Ange-hells"><img src="./image/partenaire/github.png" /></a>
+                    <a href="https://www.linkedin.com/in/nathan-thomasset-aa0b3112b"><img src="./image/partenaire/linkedin.png" /></a>
+                </fieldset>
+            </div>
         </div>
         <div id='right-block'>
             
