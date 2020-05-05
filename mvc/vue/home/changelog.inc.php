855d841182ed2fdb66159ae66d687c287e59599f
Arceus <49156328+Ange-hells@users.noreply.github.com>
2020-05-05 19:05:49 +0200

    Update changelog.inc.php

--- mvc/vue/home/changelog.inc.php
+++ mvc/vue/home/changelog.inc.php
@@ -14,42 +14,44 @@
             <div class="changelog">
                 V2.7 : Ajout de la section Confinement,</br>
                 &emsp;&emsp;&ensp;&nbsp;Remplisage de la section Confinement,</br>
-                &emsp;&emsp;&ensp;&nbsp;Ajout du lien clicable "Changelog".</br>
+                &emsp;&emsp;&ensp;&nbsp;Ajout du lien clicable "Changelog".</br></br>
                 V2.6 : Mise a jour des mention legal,</br>
                 &emsp;&emsp;&ensp;&nbsp;Ajout de Contact,</br>
                 &emsp;&emsp;&ensp;&nbsp;Ajout du tableau competence,</br>
-                &emsp;&emsp;&ensp;&nbsp;Construction et ajout des image "en cour" pour veilles.</br>
+                &emsp;&emsp;&ensp;&nbsp;Construction et ajout des image "en cour" pour veilles.</br></br>
                 V2.5 : Mise a jour du tableau fiche,</br>
                 &emsp;&emsp;&ensp;&nbsp;Mise a jour des lien interne,</br>
                 &emsp;&emsp;&ensp;&nbsp;Mise a jour des lien externe.</br></br>
-                V2.4</br>
+                V2.4 : Ajout de l'onglet et de la page de projet Owa</br>
+                &emsp;&emsp;&ensp;&nbsp;Ajout de la section Veilles</br></br>
+                V2.3 : Mise a jour du scrip et de la base de donnée mySQL</br>
                 &emsp;&emsp;&ensp;&nbsp;</br>
-                V2.3</br>
+                V2.2 : Ajout de la section Genesys</br>
+                &emsp;&emsp;&ensp;&nbsp;Ajout de la section Evasion</br></br>
+                V2.1 : Creation de la section Liste des competences</br>
                 &emsp;&emsp;&ensp;&nbsp;</br>
-                V2.2</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V2.1</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V2.0</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.8</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.7</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.6</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.5</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.4</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.3</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.2</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.1</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
-                V1.0</br>
-                &emsp;&emsp;&ensp;&nbsp;</br>
+                V2.0 : Creation de la section Fiche de competence</br>
+                &emsp;&emsp;&ensp;&nbsp;Creation du tableau Liste des fiches</br>
+                &emsp;&emsp;&ensp;&nbsp;Ajout des section CinePassion38, Keller2020, Agenda, MDI</br></br>
+                V1.8 : </br>
+                &emsp;&emsp;&ensp;&nbsp;</br></br>
+                V1.7 : </br>
+                &emsp;&emsp;&ensp;&nbsp;</br></br>
+                V1.6 : </br>
+                &emsp;&emsp;&ensp;&nbsp;</br></br>
+                V1.5 : </br>
+                &emsp;&emsp;&ensp;&nbsp;</br></br>
+                V1.4 : Creation de la base de donnee mySQL</br>
+                &emsp;&emsp;&ensp;&nbsp;</br></br>
+                V1.3 : Creation du text de presentation</br>
+                &emsp;&emsp;&ensp;&nbsp;Creation et ajout des images de la galerie d'accueil</br></br>
+                V1.2 : Remplisage du plans de site</br>
+                &emsp;&emsp;&ensp;&nbsp;Ajout des lien dans les image du pied de page</br></br>
+                V1.1 : Importation de Genesys</br>
+                &emsp;&emsp;&ensp;&nbsp;Creation et importation des image d'en-tête et de pied de page </br></br>
+                V1.0 : Initialisation du projet</br>
+                &emsp;&emsp;&ensp;&nbsp;Construction de l'arboresance </br>
+                &emsp;&emsp;&ensp;&nbsp;Creation des premieres page </br></br>
             </div>
         </div>
         <div id='right-block'>
